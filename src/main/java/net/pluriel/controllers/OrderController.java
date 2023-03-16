80c5f7369fc22130eeaa992e041f65b8d8920f5f
unknown <ibrahim.oukziz@outlook.com>
2023-03-16 12:10:39 +0100

    product add crud

--- src/main/java/net/pluriel/controllers/OrderController.java
+++ src/main/java/net/pluriel/controllers/OrderController.java
@@ -1,5 +1,7 @@
 package net.pluriel.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import net.pluriel.dto.requests.OrderRequestDto;
+import net.pluriel.dto.responses.ClientResponseDto;
 import net.pluriel.dto.responses.OrderResponseDto;
 import net.pluriel.dto.responses.OrderResponseDto;
 import net.pluriel.services.OrderService;
@@ -29,5 +32,10 @@ public class OrderController {
 	@GetMapping("/{id}")
 	public ResponseEntity<OrderResponseDto> getOne(@PathVariable(name = "id") Integer orderId){
 		return new ResponseEntity<OrderResponseDto>(orderService.getOne(orderId), HttpStatus.OK);
+	}
+	
+	@GetMapping("/all")
+	public ResponseEntity<List<OrderResponseDto>> getAll(){
+		return new ResponseEntity<List<OrderResponseDto>>(orderService.getAll(), HttpStatus.OK);
 	}
 }
